ed6db2f44e2f629272db8a830467737400692143
Nícolas Renê <nicolastonete@gmail.com>
2023-04-27 17:07:25 -0300

    release[1.0.0]: site de receitas

---
+++ js/script.js
@@ -0,0 +1,87 @@
+let image1 = document.querySelectorAll(".image")[0];
+let image2 = document.querySelectorAll(".image")[1];
+let image3 = document.querySelectorAll(".image")[2];
+let image4 = document.querySelectorAll(".image")[3];
+let image5 = document.querySelectorAll(".image")[4];
+let image6 = document.querySelectorAll(".image")[5];
+let image7 = document.querySelectorAll(".image")[6];
+let image8 = document.querySelectorAll(".image")[7];
+
+
+function mouseOver1(){
+     image1.style.opacity = "0.8"; 
+}
+
+function mouseOut1() {
+    image1.style.opacity = "1";
+}
+
+
+function mouseOver2(){
+    image2.style.opacity = "0.8";
+    
+}
+
+function mouseOut2() {
+   image2.style.opacity = "1";
+}
+
+
+function mouseOver3(){
+    image3.style.opacity = "0.8";
+    
+}
+
+function mouseOut3() {
+   image3.style.opacity = "1";
+}
+
+
+function mouseOver4(){
+    image4.style.opacity = "0.8";
+    
+}
+
+function mouseOut4() {
+   image4.style.opacity = "1";
+}
+
+
+function mouseOver5(){
+    image5.style.opacity = "0.8";
+    
+}
+
+function mouseOut5() {
+   image5.style.opacity = "1";
+}
+
+
+function mouseOver6(){
+    image6.style.opacity = "0.8";
+    
+}
+
+function mouseOut6() {
+   image6.style.opacity = "1";
+}
+
+
+function mouseOver7(){
+    image7.style.opacity = "0.8";
+    
+}
+
+function mouseOut7() {
+   image7.style.opacity = "1";
+}
+
+
+function mouseOver8(){
+    image8.style.opacity = "0.8";
+    
+}
+
+function mouseOut8() {
+   image8.style.opacity = "1";
+}
