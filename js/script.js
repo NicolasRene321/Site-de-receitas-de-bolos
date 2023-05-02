7253e5aacc27035ef44ddd184aa5b655dc1a93c5
Nícolas Renê <nicolastonete@gmail.com>
2023-05-02 18:33:14 -0300

    release[1.0.0]: site de receitas

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Decidiu-se colocar efeito de opacidade ao passar o mouse por cima de uma das imagens de bolo por meio de cósigos no JavaScript.
+// As variáveis a seguir capturam a localização das imagens a partir do DOM!
 let image1 = document.querySelectorAll(".image")[0];
 let image2 = document.querySelectorAll(".image")[1];
 let image3 = document.querySelectorAll(".image")[2];
@@ -7,16 +9,16 @@ let image6 = document.querySelectorAll(".image")[5];
 let image7 = document.querySelectorAll(".image")[6];
 let image8 = document.querySelectorAll(".image")[7];
 
-
+// Códigos para a primeira imagem
 function mouseOver1(){
-     image1.style.opacity = "0.8"; 
+     image1.style.opacity = "0.8"; // Obs: Aqui e nos código a seguir há o uso da estilização por meio do "style", assim como poderia ser feito com CSS. 
 }
 
 function mouseOut1() {
     image1.style.opacity = "1";
 }
 
-
+// Códigos para a segunda imagem
 function mouseOver2(){
     image2.style.opacity = "0.8";
     
@@ -26,7 +28,7 @@ function mouseOut2() {
    image2.style.opacity = "1";
 }
 
-
+// Códigos para a terceira imagem
 function mouseOver3(){
     image3.style.opacity = "0.8";
     
@@ -36,7 +38,7 @@ function mouseOut3() {
    image3.style.opacity = "1";
 }
 
-
+// Códigos para a quarta imagem
 function mouseOver4(){
     image4.style.opacity = "0.8";
     
@@ -46,7 +48,7 @@ function mouseOut4() {
    image4.style.opacity = "1";
 }
 
-
+// Códigos para a quinta imagem
 function mouseOver5(){
     image5.style.opacity = "0.8";
     
@@ -56,7 +58,7 @@ function mouseOut5() {
    image5.style.opacity = "1";
 }
 
-
+// Códigos para a sexta imagem
 function mouseOver6(){
     image6.style.opacity = "0.8";
     
@@ -66,7 +68,7 @@ function mouseOut6() {
    image6.style.opacity = "1";
 }
 
-
+// Códigos para a sétima imagem
 function mouseOver7(){
     image7.style.opacity = "0.8";
     
@@ -76,7 +78,7 @@ function mouseOut7() {
    image7.style.opacity = "1";
 }
 
-
+// Códigos para a oitava imagem
 function mouseOver8(){
     image8.style.opacity = "0.8";
     
